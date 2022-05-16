35cd9653187ba4592d6bc4ec99c26a6b7918b07b
Roseffc <roseffc@gmail.com>
2022-05-16 10:23:05 -0300

    aula finalizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersListComponent } from './components/pages/users/users-list/users-list.component';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UsersListComponent},
+  { path: 'form', component: UserFormComponent},
+  { path: 'form/:id', component: UserFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
